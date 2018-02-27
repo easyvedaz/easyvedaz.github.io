051db0138320918956ace5fc9d1d0e5d6c0d2b29
easyvedaz <easyvedaz@gmail.com>
2018-02-27 16:29:13 +0530

    skip api catching

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
+var CACHE_DYNAMIC_NAME = 'dynamic'
+
 self.addEventListener('install', function (event) {
   console.log('service worker -- installing service worker...', event)
 })
@@ -7,13 +9,17 @@ self.addEventListener('activate', function (event) {
 })
 
 self.addEventListener('fetch', function (event) {
+  var apiUrl = 'https://api.easyvedaz.com/'
+  if (event.request.url.indexOf(url) > -1) {
+    return fetch(event.request)
+  }
   event.respondWith(
     caches.match(event.request).then(function (response) {
       if (response) {
         return response
       } else {
         return fetch(event.request).then(function (res) {
-          caches.open('dynamic').then(function (cache) {
+          caches.open(CACHE_DYNAMIC_NAME).then(function (cache) {
             cache.put(event.request.url, res.clone())
             return res
           })
