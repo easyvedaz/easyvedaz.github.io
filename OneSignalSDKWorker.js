f8e39e10202ae3b19fe6c59c6f76aeb8236617a0
easyvedaz <easyvedaz@gmail.com>
2018-03-04 16:28:27 +0530

    seriveworker version

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'tatvacache1'
+var CACHE_NAME = 'tatvacache2'
 
 self.addEventListener('install', function (event) {
   console.log('service worker -- installing service worker...', event)
