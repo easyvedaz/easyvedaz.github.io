699dfc27309e9f54f34b899656a2882d3b8d0028
easyvedaz <easyvedaz@gmail.com>
2018-03-13 17:49:41 +0530

    google login

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,2 +1,8 @@
 importScripts('/sw5.js');
 importScripts('https://cdn.onesignal.com/sdks/OneSignalSDK.js');
+
+OneSignal.on('notificationDisplay', function (event) {
+    console.warn('OneSignal notification displayed:', event);
+    console.warn(event.id)
+    
+  });
