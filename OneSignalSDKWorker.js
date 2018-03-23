4be6f405cfa25f13e183d052de84be12fd3dfa25
easyvedaz <easyvedaz@gmail.com>
2018-03-23 11:18:27 +0530

    onesignal setup

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,8 +1,2 @@
 importScripts('/sw5.js');
 importScripts('https://cdn.onesignal.com/sdks/OneSignalSDK.js');
-
-OneSignal.on('notificationDisplay', function (event) {
-    console.warn('OneSignal notification displayed:', event);
-    console.warn(event.id)
-    
-  });
